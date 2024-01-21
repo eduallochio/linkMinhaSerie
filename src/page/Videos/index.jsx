86d184fd5a76b12211f076e583566ff033357de2
eduallochio.github.io <43136553+eduallochio@users.noreply.github.com>
2024-01-21 12:48:33 -0300

    correção do html da page videos

--- src/page/Videos/index.jsx
+++ src/page/Videos/index.jsx
@@ -1,19 +1,15 @@
-import styles from '../Video/videos.mudele.css'
+// import styles from "./Video/videos.mudele.css"
 
 function Videos () {
   return (
 
-    
-      <div>
-        <div className={styles.videos}>
-          <div className="main-video-content">
-            <video src="" controls muted></video>
-            <label className="main-info"></label>
-          </div>
-          <div className={styles.mudeles.css}></div>
-        </div>
+    <div className="container">
+      <div className="main-video-content">
+        <video src="" controls muted autoPlay></video>
+        <label className="main-info"></label>
       </div>
-    
+      <div className="playlist"></div>
+    </div>
   )
 }
 
