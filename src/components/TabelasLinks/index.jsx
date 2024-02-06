205291365f072b5592ce2c7c8fbc42dd714e3fbc
eduallochio.github.io <43136553+eduallochio@users.noreply.github.com>
2024-02-06 11:10:15 -0300

    retirada de alguns links e troca da cor de fundo

--- src/components/TabelasLinks/index.jsx
+++ src/components/TabelasLinks/index.jsx
@@ -30,38 +30,12 @@ function TabelasLinks() {
         <a
           target="_blank"
           rel="noopener noreferrer"
-          href="https://www.tiktok.com/@minhaserieemcortes"
-        >
-          Me siga do TikTok
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
           href="https://www.youtube.com/@MinhaGamePlay10"
         >
           MinhaGamePlay
         </a>
       </li>
-      <li>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.youtube.com/watch?v=LgVRYN5Sc08&list=PLvBi8LnyE4aAhUEqY3ZBssptUAsIvT9L4&pp=iAQB"
-        >
-          Bastidores de TBBT
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.youtube.com/watch?v=iHj42IhpPGA&list=PLvBi8LnyE4aBkskv_a6hViQV5uBPuvCo4&pp=iAQB"
-        >
-          Piloto não usado de TBBT
-        </a>
-      </li>
+  
     </ul>
   )
 }
